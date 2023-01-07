2e9817b48cce6852a091ebc2149bb7f0c233c2a3
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2023-01-07 13:22:25 -0400

    optimiza el codigo- mejor forma de obtener la hora

--- main.js
+++ main.js
@@ -6,9 +6,7 @@ let secondAct = tiempoActual.getSeconds();
 
 const dayString = document.querySelector('.day-string');
 const dateInfo = document.querySelector('.date-info');
-const hour = document.querySelector('.hour');
-const minute = document.querySelector('.minute');
-const second = document.querySelector('.second');
+const time = document.querySelector('.time');
 
 const months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre',
 'Octubre','Noviembre','Diciembre'];
@@ -21,43 +19,10 @@ const setDate = ()=>{
     +tiempoActual.getFullYear();
 }
 
-const timeWithZero =(t)=>{
-    if(t < 10) t = '0' + `${t}`;
-    return t
-}
-
-const setTime=(v,cb)=>{
-    v++;
-    if(v == 60){
-        v = 0; 
-        cb();
-    }
-    v = timeWithZero(v);
-    return v;
-}
-
-const setHours = ()=>{
-    hourAct++;
-    if(hourAct == 24) hourAct = 0 ;
-    timeWithZero(hourAct);
-    hour.textContent = hourAct;
-}
-
-const setMinutes=()=>{
-    minuteAct = setTime(minuteAct,setHours); 
-    minute.textContent = minuteAct;
-}
-
-const setSeconds =()=>{
-    secondAct = setTime(secondAct,setMinutes);
-    console.log(secondAct);
-    second.textContent = secondAct;
-}
-
 addEventListener('load',()=>{
     setDate();
-    hour.textContent = timeWithZero(hourAct);
-    minute.textContent = timeWithZero(minuteAct);  
-    second.textContent = timeWithZero(secondAct);
-    setInterval(() => {setSeconds()}, 1000);
+    setInterval(() => {
+        const hour = new Date();
+        time.textContent = hour.toLocaleTimeString();
+    }, 1000);
 });
